f1a13fc96aeae1caf96bd9944378b01cb3879b53
bayerlumf <113850619+bayerlumf@users.noreply.github.com>
2022-11-15 22:17:36 +0100

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -5,9 +5,7 @@ This module creates and configures the Flask app and sets up the logging
 and SQL database
 """
 import sys
-from flask import Flask
-from flask_talisman import Talisman
-from flask_cors import CORS
+
 from service import config
 from service.common import log_handlers
 
